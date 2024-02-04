35cfe816cd0a9ea157e1a70e0949a7e3afab8deb
ffletcherr <homayouneemoradi@gmail.com>
2024-02-04 16:19:09 +0330

    add logger and optimizer

--- train.py
+++ train.py
@@ -3,17 +3,36 @@ from pathlib import Path
 
 import cv2
 import numpy as np
+import torch
 from torch.utils.data import DataLoader
 
 from data_utils import OceanDataset
 from et_track import ET_Tracker
 from lib.core.config_ocean import config
 from lib.core.function import ocean_train
+from lib.utils.utils import create_logger
 from parameters import parameters
+
+
+def check_trainable(model, logger):
+    """
+    print trainable params info
+    """
+    trainable_params = [p for p in model.parameters() if p.requires_grad]
+    logger.info("trainable params:")
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            logger.info(name)
+
+    assert len(trainable_params) > 0, "no trainable parameters"
+
+    return trainable_params
+
 
 if __name__ == "__main__":
 
     gpu_num = 1
+    logger, _, tb_log_dir = create_logger(config, "OCEAN", "train")
     dataset_path = Path(os.environ["dataset_path"])
     dataset = OceanDataset(
         cfg=config,
@@ -51,3 +70,10 @@ if __name__ == "__main__":
     params = parameters()
     model = params.net
     model.initialize(params.model_name, checkpoint_epoch=params.checkpoint_epoch)
+    trainable_params = check_trainable(model, logger)
+    optimizer = torch.optim.SGD(
+        trainable_params,
+        config.OCEAN.TRAIN.LR,
+        momentum=config.OCEAN.TRAIN.MOMENTUM,
+        weight_decay=config.OCEAN.TRAIN.WEIGHT_DECAY,
+    )
