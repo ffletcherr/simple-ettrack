93d1aabcec73cfcd2b760e6948accbc801d519c7
ffletcherr <homayouneemoradi@gmail.com>
2024-02-04 16:08:20 +0330

    init model

--- train.py
+++ train.py
@@ -6,8 +6,10 @@ import numpy as np
 from torch.utils.data import DataLoader
 
 from data_utils import OceanDataset
+from et_track import ET_Tracker
 from lib.core.config_ocean import config
 from lib.core.function import ocean_train
+from parameters import parameters
 
 if __name__ == "__main__":
 
@@ -36,3 +38,16 @@ if __name__ == "__main__":
         reg_weight,
         bbox,
     ) = batch
+
+    model = ET_Tracker(
+        search_size=256,
+        template_size=128,
+        stride=16,
+        e_exemplars=4,
+        sm_normalization=True,
+        temperature=2,
+        dropout=False,
+    )
+    params = parameters()
+    model = params.net
+    model.initialize(params.model_name, checkpoint_epoch=params.checkpoint_epoch)
